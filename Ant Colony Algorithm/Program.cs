46a0f9ba2951a5410baacc6f5048d55427046f54
Pasha Technik <pacha120431@gmail.com>
2021-04-10 19:34:22 +0300

    Initial commit

---
+++ Ant Colony Algorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ant_Colony_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
